e75298492df6be001a1318d5b60cf508877c8a37
Yash Priyadarshi <yashp4973@gmail.com>
2024-03-20 23:52:15 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,10 +1,14 @@
 package demo;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import java.util.logging.Level;
@@ -17,7 +21,7 @@ public class TestCases {
     {
         System.out.println("Constructor: TestCases");
 
-        WebDriverManager.chromedriver().timeout(30).setup();
+        //WebDriverManager.chromedriver().timeout(30).setup();
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
 
@@ -48,9 +52,112 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
+        driver.get("https://leetcode.com/");
+        //driver.navigate().to("https://leetcode.com/");
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+        if(URL.contains("leetcode")){
+            System.out.println("URL contains LeetCode in it");
+        }else{
+            System.out.println("URL doesnot contains LeetCode in it");
+        }
+        System.out.println("end Test case: testCase01");
+    }
+
+
+    public  void testCase02() throws InterruptedException{
+        System.out.println("Start Test case: testCase02");
+        driver.get("https://leetcode.com/");
+
+        WebElement questions = driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        questions.click();
+        Thread.sleep(2000);
+
+        String URL = driver.getCurrentUrl();
+        if(URL.contains("problemset")){
+            System.out.println("URL contains problemset in it");
+        }else{
+            System.out.println("URL doesnot contains problemset in it");
+        }
+
+        
+        for(int i=1; i<6 ; i++){
+            List<WebElement> questionLists = driver.findElements(By.xpath("//div[contains(@class,'odd:bg-layer-1')]//div[@class='truncate']"));
+            String questionList =questionLists.get(i).getText();
+            System.out.println("Question From List---- " + questionList);
+            questionLists.get(i).click();
+            Thread.sleep(2000);
+            String title = driver.getTitle();
+            System.out.println("Title of the Page ----" + title);
+            String questionName = questionList.substring(3);
+            if(title.contains(questionName)){
+                System.out.println("Question Name matches the Title");
+            }else{
+                System.out.println("Question Name does not matches the Title");
+            }
+            driver.navigate().back();
+            Thread.sleep(2000);
+        }
+
         System.out.println("end Test case: testCase02");
     }
 
+
+    public  void testCase03() throws InterruptedException{
+        System.out.println("Start Test case: testCase03");
+        driver.get("https://leetcode.com/");
+        
+        WebElement questions = driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        questions.click();
+        Thread.sleep(2000);
+        
+        WebElement twoSum = driver.findElement(By.xpath("//a[@href='/problems/two-sum']"));
+        twoSum.click();
+
+        String URL = driver.getCurrentUrl();
+        System.out.println("URL ----- " + URL);
+        if(URL.contains("two-sum")){
+            System.out.println("URL contains two-sum in it");
+        }else{
+            System.out.println("URL doesnot contains two-sum in it");
+        }
+        System.out.println("end Test case: testCase03");
+    }
+
+
+    public  void testCase04() throws InterruptedException{
+        System.out.println("Start Test case: testCase04");
+        driver.get("https://leetcode.com/");
+        
+        WebElement questions = driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        questions.click();
+        Thread.sleep(2000);
+        
+        WebElement twoSum = driver.findElement(By.xpath("//a[@href='/problems/two-sum']"));
+        twoSum.click();
+
+        driver.findElement(By.xpath("//div[@id='submissions_tab']")).click();
+        // Actions actions = new Actions(driver);
+        // actions.moveToElement(submit).perform();
+        // Thread.sleep(2000);
+
+        String URL = driver.getCurrentUrl();
+        if(URL.contains("submissions")){
+            System.out.println("URL contains submissions in it");
+        }else{
+            System.out.println("URL doesnot contains submissions in it");
+        }
+        WebElement text = driver.findElement(By.xpath("//a[text()='Register or Sign In']"));
+        String str = text.getText();
+        System.out.println(str);
+        
+        if(text.isDisplayed()){
+            System.out.println("Register or Sign In is displayed");
+        }else{
+            System.out.println("Register or Sign In is not displayed");
+        }
+  
+        System.out.println("end Test case: testCase04");
+    }
 
 }
